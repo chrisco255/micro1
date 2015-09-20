ecfdd44874ac267b102eae7714ad9c56d5388296
ChrisCo <ufcordle@hotmail.com>
2015-09-20 04:29:27 -0400

    ll

--- production.js
+++ production.js
@@ -6,10 +6,12 @@
 module.exports = {
 	// MongoDB connection options
 	mongo: {
-		uri: process.env.MONGOLAB_URI
+		uri: 	process.env.MONGOLAB_URI ||
+					process.env.CUSTOMCONNSTR_MONGOLAB_URI
 	},
 	servicebus: {
-		uri: process.env.SERVICEBUS_URI
+		uri: 	process.env.SERVICEBUS_URI ||
+					process.env.CUSTOMCONNSTR_SERVICEBUS_URI
 	},
 	port: process.env.PORT || 8080
 };
